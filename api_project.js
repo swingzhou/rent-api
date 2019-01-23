101cdbc43531a25ce51b0540560e86de1d761825
swingzhou <337043944@qq.com>
2019-01-23 10:06:53 +0800

    no message

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "xlink-rent-rest",
+  "version": "0.0.1",
+  "description": "租赁业务服务接口",
+  "title": "xlink-rent-rest",
+  "url": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2019-01-17T04:38:52.954Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
